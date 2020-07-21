db57e937e68bb6e2220aba16d48a540e0a6e4aec
Kelly Mason <mason.kelly.m@gmail.com>
2020-07-20 19:01:14 -0600

    first commit

---
+++ quiz-server/src/routers/question.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const Question = require('../models/question')
+const router = new express.Router()
+
+router.post('/questions', async (req, res) => {
+  const question = new Question({
+    ...req.body
+  })
+  try{ 
+    await question.save();
+    res.status(201).send(question);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e.message);
+  }
+})
+
+router.get('/questions', async (req, res) => {
+  try {
+    let questions = await Question.find();
+    res.send(questions);
+  } catch(e) {
+    res.status(500).send(e.message);
+  }
+})
+
+router.get('/questions/:id', async (req, res) => {
+  const _id = req.params.id;
+  try {
+    // todo: implement lookup in db
+    const question = {
+      _id: 1,
+      question: 'Is this a hard-coded question from the server?',
+      correctAnswer: 'Yes!'
+    }
+  } catch(e) {
+    res.status(500).send(e.message);
+  }
+})
+
+module.exports = router
